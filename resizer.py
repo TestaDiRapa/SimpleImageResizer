cde1dce292b7fc4a13e2e7875129376a9c531a2a
TestaDiRapa <v.c.pierro@gmail.com>
2018-11-13 22:38:07 +0100

    Add files via upload

---
+++ resizer.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+import shutil
+import sys
+
+ratio = (32,32)
+
+def resize_recursive(in_path, out_path):
+    for file in os.listdir(in_path):
+        if os.path.isdir(in_path+"/"+file):
+            os.mkdir(out_path+"/"+file)
+            resize_recursive(in_path+"/"+file, out_path+"/"+file)
+        if file[-3:] == "png":
+            image = Image.open(in_path+"/"+file)
+            image.thumbnail(ratio)
+            image.save(out_path+"/"+file, "PNG")
+            
+if __name__ == '__main__':
+    shutil.rmtree('out', ignore_errors=True)
+    os.mkdir("out")
+    resize_recursive(sys.argv[1], "out")
+    
